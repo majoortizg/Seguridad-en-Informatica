73400b759fbc4afe319a3b4c97fb475c806a063e
majoortizg <157551392+majoortizg@users.noreply.github.com>
2025-02-07 03:19:46 +0000

    primer committt

---
+++ elgamal.py
@@ -0,0 +1,24 @@
+# Algoritmo ElGamal
+
+import random 
+import Crypto.Util.number
+
+# Usamos el primo estandar RFC 3526 de 1536 bits MODP
+# usamos el primo estandar RFC 3526 de 1536 bit MODP Group
+p = int("FFFFFFFFFFFFFFFFC90FDAA22168C234C4C6628B80DC1CD129024E088A67CC74020BBEA6 3B139B22514A08798E3404DDEF9519B3CD3A431B302B0A6DF25F14374FE1356D6D51C245E485B576625E7EC6F44C42E9A637ED6B0BFF5CB6F406B7EDEE386BFB5A899FA5AE9F24117C4B1FE649286651ECE45B3DC2007CB8A163BF0598DA48361C55D39A69163FA8FD24CF5F83655D23DCA3AD961C62F356208552BB9ED529077096966D670C354E4ABC9804F1746C08CA237327FFFFFFFFFFFFFFFF",16)
+
+# Usamos el generador estandar de RFC 3526
+g = 2
+
+# Generar las llaves privadas
+bob = random.getrandbits(256)
+alice = random.getrandbits(256)
+
+# Calculamos y
+# y = g^bob mod pass
+y = pow(g,bob,p)
+print("\n y:", y)
+print("\n Llave publica de Bob: (p,g,y)", p,g,y)
+
+M = 850
+print("\n Mensaje Original: ", M)
---
+++ rsa1.py
@@ -0,0 +1,30 @@
+# Ejemplo de cifrado usando RSA
+
+import Crypto.Random
+import Crypto.Util.number
+
+# Vamos a usar 2^16 + 1 = 65537, el numero 4 de Fermat por eficienia
+# Debemos tomar en cuenta que "e" sera parte de la llave publica por eso
+# no hay problema de usar el numero de Fermat
+e = 65537
+print("\n", "Numero de Fermat 2^16+1 e: ",e)
+
+
+# Calculamos las llaves publicas de Alice
+pA = Crypto.Util.number.getPrime(1024, randfunc = Crypto.Random.get_random_bytes)
+print("\n", "RSA - Primo de Alice pA: ", pA)
+qA = Crypto.Util.number.getPrime(1024, randfunc = Crypto.Random.get_random_bytes)
+print("\n", "RSA - Primo de Alice qA: ", qA)
+
+# Calculamos nA = pA * qA
+nA = pA * qA
+print("\n", "RSA - nA: ", nA)
+
+# Calculamos phiA
+phiA = (pA - 1) * (qA - 1)
+print ("\n", "RSA - phiA: ", phiA)
+
+# Calculamos la Llave Privada de Alice
+dA = Crypto.Util.number.inverse(e,phiA)
+print("\n", "RSA Llave Privada - dA: ", dA)
+
